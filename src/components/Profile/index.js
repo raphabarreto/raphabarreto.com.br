b81f97ab73dcfcabe7087ff8fcbe650bbe748215
Raphael Barreto <tnt.raphael@gmail.com>
2020-04-26 18:44:35 -0300

    Criado componente e compartilhando entre páginas

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Raphael Barreto</h1>
+    <h2>Desenvolvedor React</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  </div>
+)
+
+export default Profile
