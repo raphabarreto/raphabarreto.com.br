aa62f38bb146a8e4f456c35c174675f3bf184927
Raphael Barreto <tnt.raphael@gmail.com>
2020-05-05 20:24:41 -0300

    refactor: refatorado o styled components
    Criado um arquivo chamado styled.js para colocar todas as estilizações de uma forma separada para
    ter mais organização e separação entre os componentes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
 import GlobalStyles from '../../styles/global';
+
+import * as S from './styled';
+
 import Profile from '../Profile';
-
-const LayoutWrapper = styled.section`
-  display: flex;
-`;
-
-const LayoutMain = styled.main`
-  background: #16202c;
-  min-height: 100vh;
-  padding: 0 3.75rem 0 20rem;
-  width: 100%;
-`;
 
 const Layout = ({ children }) => {
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
       <GlobalStyles />
       <aside>
         <Profile />
       </aside>
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWrapper>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   );
 };
 
