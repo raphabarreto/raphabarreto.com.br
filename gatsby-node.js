d2b74e6702c199c53008b45ca548f5d159446ad9
Raphael Barreto <tnt.raphael@gmail.com>
2020-05-16 14:34:42 -0300

    fix: corrigido path

--- gatsby-node.js
+++ gatsby-node.js
@@ -52,7 +52,7 @@ exports.createPages = ({ graphql, actions }) => {
   `).then((result) => {
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
       createPage({
-        path: node.fields.slugs,
+        path: node.fields.slug,
         component: path.resolve('./src/templates/blog-post.js'),
         context: {
           slug: node.fields.slug,
