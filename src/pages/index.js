9a869a0434c8d9df42c1e0f37b2d5b1ff8e493a3
Raphael Barreto <tnt.raphael@gmail.com>
2020-05-16 14:07:50 -0300

    feat: adicionado fields com o `onCreateNode`
    Adicionado fields de slug na query do GraphQL para quando clicar no post, aparecer o título do post
    como endereço na URL

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,9 @@ const IndexPage = () => {
         allMarkdownRemark {
           edges {
             node {
+              fields {
+                slug
+              }
               frontmatter {
                 background
                 category
@@ -37,11 +40,12 @@ const IndexPage = () => {
           node: {
             frontmatter: { background, category, date, description, title },
             timeToRead,
+            fields: { slug },
           },
         }) => (
           <PostItem
             key={date}
-            slug="/about/"
+            slug={slug}
             background={background}
             category={category}
             date={date}
