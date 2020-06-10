d27a3729a27b2680d677bfbf7b1f62bbd07e2c1e
Raphael Barreto <tnt.raphael@gmail.com>
2020-06-10 12:27:56 -0300

    fix: corrigido query que retornava o date_timestamp como null

--- src/utils/algolia_queries.js
+++ src/utils/algolia_queries.js
@@ -1,8 +1,6 @@
 // @ts-nocheck
-const postsQuery = `{
-  posts: allMarkdownRemark(
-    sort: { fields: frontmatter___date, order: DESC }
-  ) {
+const postQuery = `{
+  posts: allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }){
     edges {
       node {
         objectID: id
@@ -10,10 +8,11 @@ const postsQuery = `{
           slug
         }
         frontmatter {
+          title
           category
+          date_timestamp: date
           date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
           description
-          title
         }
         excerpt(pruneLength: 5000)
       }
@@ -24,22 +23,19 @@ const postsQuery = `{
 const flatten = (arr) =>
   arr.map(({ node: { frontmatter, ...rest } }) => ({
     ...frontmatter,
-
     date_timestamp: parseInt(
       (new Date(frontmatter.date_timestamp).getTime() / 1000).toFixed(0)
     ),
     ...rest,
   }));
+const settings = { attributesToSnippet: [`excerpt:20`] };
 
 const queries = [
   {
-    query: postsQuery,
-
+    query: postQuery,
     transformer: ({ data }) => flatten(data.posts.edges),
     indexName: `Posts`,
-    settings: {
-      attributesToSnippet: ['excerpt: 20'],
-    },
+    settings,
   },
 ];
 
