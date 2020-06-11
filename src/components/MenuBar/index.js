32b8b609dc7c059db8c4452a0e1e302651a047f4
Raphael Barreto <tnt.raphael@gmail.com>
2020-06-10 21:56:35 -0300

    feat: criado método para troca de visualização

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -6,17 +6,23 @@ import {
   UpArrowAlt as Arrow,
   Bulb as Light,
 } from 'styled-icons/boxicons-regular';
+import { ThList as List } from 'styled-icons/fa-solid';
 
 import * as S from './styled';
 
 const MenuBar = () => {
   const [theme, setTheme] = useState(null);
+  const [display, setDisplay] = useState(null);
 
   const isDarkMode = theme === 'dark';
+  const isListMode = display === 'list';
 
   useEffect(() => {
     setTheme(window.__theme);
+    setDisplay(window.__display);
+
     window.__onThemeChange = () => setTheme(window.__theme);
+    window.__onDisplayChange = () => setDisplay(window.__display);
   }, []);
 
   return (
@@ -43,8 +49,13 @@ const MenuBar = () => {
         >
           <Light />
         </S.MenuBarItem>
-        <S.MenuBarItem title="Mudar visualização">
-          <Grid />
+        <S.MenuBarItem
+          title="Mudar visualização"
+          onClick={() => {
+            window.__setPreferredDisplay(isListMode ? 'grid' : 'list');
+          }}
+        >
+          {isListMode ? <Grid /> : <List />}
         </S.MenuBarItem>
         <S.MenuBarItem title="Ir para o Topo">
           <Arrow />
